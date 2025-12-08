231cc9bab0ee7f1521f490aa77ad721bda309e3f
joeperpetua <qwerty.perpetua@gmail.com>
2025-12-08 03:12:59 +0100

    feat: update commits & PRs endpoint to get all data available for a user
    Previously it was capped to the last year data

--- src/app/api/repoUserContribution/_types.ts
+++ src/app/api/repoUserContribution/_types.ts
@@ -1,49 +1,83 @@
+type GqlPaginationVars = {
+	after?: string | null;
+	first?: number;
+};
+
+// Update your function signature to accept these extra vars
+export type AnyGqlVar = (GqlStartCountVar | GqlContributionVar) &
+	GqlPaginationVars;
+
+export interface Connection<T> {
+	nodes: T[];
+	pageInfo: {
+		hasNextPage: boolean;
+		endCursor: string | null;
+	};
+}
+
 export interface GqlStartCountVar {
-  owner: string;
-  repo: string;
+	owner: string;
+	repo: string;
 }
 
 export interface GqlContributionVar {
-  user: string;
+	user: string;
+	from?: string;
+	to?: string;
 }
 
 export interface ContributionByRepo {
-  repository: { nameWithOwner: string };
-  contributions: { totalCount: number };
+	repository: { nameWithOwner: string };
+	contributions: { totalCount: number };
 }
 
 export interface GqlStarCount {
-  repository: {
-    owner: { login: string },
-    name: string, 
-    stargazerCount: number,
-  };
+	repository: {
+		owner: { login: string };
+		name: string;
+		stargazerCount: number;
+	};
+}
+
+export interface GqlUserCreationDate {
+	user: {
+		createdAt: string;
+	};
 }
 
 export interface GqlUserCommits {
-  user: {
-    contributionsCollection: {
-      commitContributionsByRepository: ContributionByRepo[]
-    }
-  }
+	user: {
+		contributionsCollection: {
+			commitContributionsByRepository: ContributionByRepo[];
+		};
+	};
+}
+
+export interface UsePRNode {
+	state: "MERGED" | "OPEN" | "CLOSED";
+	repository: { nameWithOwner: string };
 }
 
 export interface GqlUserPRs {
-  user: {
-    contributionsCollection: {
-      pullRequestContributionsByRepository: ContributionByRepo[]
-    }
-  }
+	user: {
+		pullRequests: {
+			nodes: UsePRNode[];
+			pageInfo: {
+				hasNextPage: boolean;
+				endCursor: string;
+			};
+		};
+	};
 }
 
 export interface GqlError {
-  type: string;
-  path: string[];
-  locations: { line: number, column: number }[];
-  message: string;
+	type: string;
+	path: string[];
+	locations: { line: number; column: number }[];
+	message: string;
 }
 
 export interface GqlResponse<T> {
-  data: T;
-  errors?: GqlError[];
+	data: T;
+	errors?: GqlError[];
 }
